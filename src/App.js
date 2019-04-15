a3ce51b8ec0c7fb43fa9c3299361ef735f032acc
Alin Simion <simion.g.alin@gmail.com>
2019-04-16 00:44:15 +0300

    Configured the API calls

--- src/App.js
+++ src/App.js
@@ -4,9 +4,36 @@ import {recipes} from './tempList';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
-
 class App extends Component {
+  state = {
+    recipes: [],
+    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_RECIPES_KEY}`
+  };
+
+  //Async/await allows me to write my code like I would be performing actions
+  //syncrhronously
+  async getRecipes() {
+    try {
+      //First I get the data and then I transfer it to JSON format
+      const data = await fetch(this.state.url);    
+      const jsonData = await data.json();
+      this.setState({
+      //The key below is the second key that I get from the API
+      //After that I set the recipes key from state to that value
+      recipes: jsonData.recipes
+      });
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    console.log("Component is mounted..");
+    this.getRecipes();
+  }
+
   render() {
+    console.log(this.state.recipes);
     return (
       <React.Fragment>
         <RecipeList />
