7c5bb9ce5450a265dd21566d9f0d96f468a27ecd
Alin Simion <simion.g.alin@gmail.com>
2019-04-16 00:54:23 +0300

    Commented the code out in order to not waste API calls

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+//The recipes import below is used as a placeholder for
+//the API because food2fork API free plan has only 50
+//requests per day
 import {recipes} from './tempList';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
   state = {
-    recipes: [],
+    //recipes: [], 
+    recipes: recipes, //placeholder for App debugging
     url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_RECIPES_KEY}`
   };
 
-  //Async/await allows me to write my code like I would be performing actions
-  //syncrhronously
-  async getRecipes() {
-    try {
-      //First I get the data and then I transfer it to JSON format
-      const data = await fetch(this.state.url);    
-      const jsonData = await data.json();
-      this.setState({
-      //The key below is the second key that I get from the API
-      //After that I set the recipes key from state to that value
-      recipes: jsonData.recipes
-      });
-    } catch(error) {
-      console.log(error);
-    }
-  }
+//  //Async/await allows me to write my code like I would be performing actions
+//  //synchronously
+//  async getRecipes() {
+//  try {
+//    const data = await fetch(this.state.url); //First get the data (await)
+//    const jsonData = await data.json(); //Then wait for that data in order to use it
+//    this.setState({
+//    //The key below is the second key that I get from the API
+//    //After that I set the recipes key from state to that value
+//    recipes: jsonData.recipes
+//    });
+//  } catch(error) {
+//    console.log(error);
+//  }
+//}
 
-  componentDidMount() {
-    console.log("Component is mounted..");
-    this.getRecipes();
-  }
+//  componentDidMount() {
+//    console.log("Component is mounted..");
+//    this.getRecipes();
+//  }
 
   render() {
     console.log(this.state.recipes);
